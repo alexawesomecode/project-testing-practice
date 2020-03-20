d8fbf00977ddbe48b010073d03ba79ab43ed38eb
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-03-20 21:45:51 +0100

    [Feature] Create ceaser-cipher function and Add tests

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,7 +1,6 @@
 import capitalize from './capitalize'
 
 test('return first letter capitalized', () => {
-
     expect(capitalize("alejandro")).toBe("Alejandro");
-
+    expect(capitalize("michgolden")).toBe("Michgolden");
 })
